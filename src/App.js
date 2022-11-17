8be95fc9c1de6386ad23015a4dc520e101e50a28
guneysrhat <guneysrhat@gmail.com>
2022-11-17 13:24:01 +0300

    handle sumbit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import { useState } from "react";
 import "./App.css";
 import Contacts from "./components/contacts/Contacts";
 import FormComponent from "./components/form/FormComponent";
 
+const initialValues = {
+  username: "",
+  phoneNumber: "",
+  gender: "",
+};
 function App() {
+  const [info, setInfo] = useState(initialValues);
+
+  const handleSumbit = () => {};
+
   return (
     <div className="App">
-      <FormComponent />
+      <FormComponent
+        info={info}
+        setInfo={setInfo}
+        handleSumbit={handleSumbit}
+      />
       <Contacts />
     </div>
   );
